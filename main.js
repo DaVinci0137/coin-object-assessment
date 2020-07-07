6608f36c65982e324ae8d182f599bfc955ba3b37
Andrew Brown <DaVinci0137@gmail.com>
2020-07-07 11:43:02 -0400

    Updated images, Completed main. js

--- main.js
+++ main.js
@@ -1,21 +1,52 @@
 let coin = {
     state: 0,
     flip: function () {
-        /* 1. Randomly set your coin object's "state" property to be either 
-           0 or 1: use "this.state" to access the "state" property on this object.*/
+        if (Math.floor(Math.random() * 2)) {
+            this.state = 0
+        } else {
+            this.state = 1
+        }
     },
+
     toString: function () {
-        /* 2. Return the string "Heads" or "Tails", depending on whether
-           "this.state" is 0 or 1. */
+        if (this.state === 1) {
+            return "Heads"
+        } else {
+            return "Tails"
+        }
     },
+
     toHTML: function () {
         let image = document.createElement('img');
-        /* 3. Set the properties of this image element to show either face-up
-           or face-down, depending on whether this.state is 0 or 1.*/
+        if (this.state === 1) {
+            image.src = "./images/heads.jpg"
+        } else {
+            image.src = "./images/tails.jpg"
+        }
+        image.width = 100
+
         return image;
     }
 };
 
-function display20FLips() { }
+console.log(coin.toString())
+console.log(coin.toHTML())
 
-function display20Images() { }
+display20Flips()
+display20Images()
+
+function display20Flips() {
+    for (i = 0; i < 20; i++) {
+        coin.flip();
+        let string = coin.toString();
+        document.body.append(string, " ");
+    }
+}
+
+function display20Images() {
+    for (i = 0; i < 20; i++) {
+        coin.flip();
+        let index = coin.toHTML();
+        document.body.append(index, " ")
+    }
+}
